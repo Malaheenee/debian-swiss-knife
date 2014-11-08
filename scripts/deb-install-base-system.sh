813a6502fd8896ed045419a76b9ca0d56483795c
Charles Malaheenee <malaheenee@gmx.fr>
2014-11-08 08:44:11 +0600

    Fixes in deb-install-base-system.sh

--- scripts/deb-install-base-system.sh
+++ scripts/deb-install-base-system.sh
@@ -9,7 +9,7 @@ E_NOTROOT=101
 # APT vars
 APT_PROGRAM="apt-get"
 APT_COMMAND="install"
-APT_OPTIONS="-qqys"
+APT_OPTIONS="-qqy"
 APT_PACKAGES=""
 APT_SOURCES_FILE="/etc/apt/sources.list"
 
@@ -26,7 +26,7 @@ runs_as_root() {
 # Update packages list
 is_apt_configured() {
     local APT_COMMAND="update"
-    local APT_OPTIONS="-s"
+    local APT_OPTIONS="-qq"
 
     if [ -e ${APT_SOURCES_FILE} ]; then
         if [ -n "$(grep -oE "^deb\W*(h|f)tt?p.*main\W*contrib\W*non-free$" ${APT_SOURCES_FILE})" ]; then
@@ -35,24 +35,22 @@ is_apt_configured() {
             echo "Done."
             return 0  
         else
-            return 1
+            echo "You need to configure apt first."
+            exit 1
         fi
     else
-        echo "File ${APT_SOURCES_FILE} missing: installaton aborted"
-        return 1
+        echo "File ${APT_SOURCES_FILE} missing: installaton aborted."
+        exit 1
     fi
 }
 
 # Select packages for installation
 user_selection() {
-    local response
-
-    read -p "What you want to install: [b]ase system, [f]onts, [a]all (default \"a\"): " $response
+    read -p "What you want to install: [b]ase system, [f]onts, [a]all (default \"a\"): " response
     case ${response} in
-        [a]* ) return 0 ;;
-        [b]* ) return 1 ;;
-        [f]* ) return 2 ;;
-        * ) return 0 ;;
+        [Aa]*|"" ) return 0 ;;
+        [Bb]* ) return 1 ;;
+        [Ff]* ) return 2 ;;
     esac
 }
 
@@ -72,8 +70,6 @@ ttf-mscorefonts-installer"
 
 # Install packages
 proceed_to_install() {
-    local response
-    
     case ${1} in
         [0]* ) install_base; install_fonts ;;
         [1]* ) install_base ;;
@@ -81,21 +77,22 @@ proceed_to_install() {
         * ) echo "Something wrong: installaton aborted."; exit 1;;
     esac
 
-    read -p "You proceed to install ... packages. Do you want to continue? [Y]es, [No] (default \"y\"): " $response
+    read -p "You proceed to install selected packages. Do you want to continue? [Y]es, [N]o (default \"yes\"): " response
     case ${response} in
-        [Yy]* ) ;;
-        [Nn]* ) echo "Installaton aborted by user."; exit 1 ;;
+        [Yy]*|"" ) 
+            is_apt_configured
+            echo -n "Installation of packages... "
+            eval ${APT_PROGRAM} ${APT_COMMAND} ${APT_OPTIONS} ${APT_PACKAGES}
+            echo "Done."
+            exit 0
+        ;;
+        [Nn]* )
+            echo "Installaton aborted by user."
+            exit 1
+        ;;
     esac
 
-    is_apt_configured
-    case ${?} in
-        [0]* ) echo -n "Installation of packages... ";
-               eval ${APT_PROGRAM} ${APT_COMMAND} ${APT_OPTIONS} ${APT_PACKAGES};
-               echo "Done." ;;
-        [1]* ) echo "You need to configure apt first." ;;
-    esac
-
-    exit 0
+    echo "xxx"
 }
 
 # Run this script
