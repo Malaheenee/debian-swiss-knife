e39db2e8677b3f4cb7946763c61e25cf25c78cc9
Charles Malaheenee <malaheenee@gmx.fr>
2014-11-13 07:45:38 +0600

    Add GUI installation

--- scripts/deb-install-base-system.sh
+++ scripts/deb-install-base-system.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# This script installs all basic packages
+# This script installs Install all basic and GUI packages (XFCE, GNOME, KDE)
+# without annoying dependencies;
 
 # Common vars
 ROOT_UID=0
@@ -12,6 +13,7 @@ APT_COMMAND="install"
 APT_OPTIONS="-qqy"
 APT_PACKAGES=""
 APT_SOURCES_FILE="/etc/apt/sources.list"
+PKG_SELECT=""
 
 # Verify user uid
 runs_as_root() {
@@ -33,7 +35,7 @@ is_apt_configured() {
             echo -n "Updating packages list... "
             eval ${APT_PROGRAM} ${APT_COMMAND} ${APT_OPTIONS}
             echo "Done."
-            return 0  
+            return 0
         else
             echo "You need to configure apt first."
             exit 1
@@ -46,38 +48,84 @@ is_apt_configured() {
 
 # Select packages for installation
 user_selection() {
-    read -p "What you want to install: [b]ase system, [f]onts, [a]all (default \"a\"): " response
+    read -p "What you want to install first: [b]ase system, [k]de, [x]fce, [m]ixed (default \"mixed\"): " response
     case ${response} in
-        [Aa]*|"" ) return 0 ;;
-        [Bb]* ) return 1 ;;
-        [Ff]* ) return 2 ;;
+        B|b ) PKG_SELECT+="base" ;;
+        K|k ) PKG_SELECT+="base kde" ;;
+        M|m|"" ) PKG_SELECT+="base mixed" ;;
+        X|x ) PKG_SELECT+="base xfce" ;;
+        [^BbKkMmXx]* ) echo "Wrong selection. Aborted."; exit 1 ;;
     esac
+
+    if [[ ${PKG_SELECT} =~ "kde" ]]; then
+        read -p "What display manager you want to use: [l]ightdm or [k]dm, (default \"lightdm\"): " response
+        case ${response} in
+            K|k* ) PKG_SELECT+=" kdm" ;;
+            L|l*|"" ) PKG_SELECT+=" lightdm" ;;
+            [^KkLl]* ) echo "Wrong selection. Aborted."; exit 1 ;;
+        esac
+    elif [[ ${PKG_SELECT} =~ xfce|mixed ]]; then
+        PKG_SELECT+=" lightdm"
+    fi
+    set_pkgs_list ${PKG_SELECT}
 }
 
-# Set variables for basic system installation
-install_base() {
-    APT_PACKAGES+=" bash-completion alsa-utils dbus ntfs-3g \
-dosfstools mtools mlocate intel-microcode bzip2 zip unzip p7zip-full \
-xz-utils libiso9660-8 unrar policykit-1 acpi-support"
-}
+# Set list of needed packages
+set_pkgs_list() {
+    if [ ${1} == "base" ]; then
+        APT_PACKAGES+=" bash-completion alsa-utils dbus ntfs-3g \
+        dosfstools mtools mlocate intel-microcode bzip2 zip unzip p7zip-full \
+        xz-utils libiso9660-8 unrar policykit-1 acpi-support"
+    fi
+    
+    if [ -n "${2}" ]; then
+        APT_PACKAGES+=" xfonts-base xfonts-scalable xserver-common xinit \
+        xserver-xorg-video-fbdev xserver-xorg-video-vesa \
+        xserver-xorg-input-evdev libnotify-bin xdg-utils xdg-user-dirs \
+        cups-bsd lsb-release tango-icon-theme dmz-cursor-theme"
 
-# Set variables for fonts installation
-install_fonts() {
-    APT_PACKAGES+=" fonts-dejavu-core fonts-dejavu-extra fonts-droid \
-fonts-liberation fonts-freefont-ttf fonts-croscore fonts-linuxlibertine \
-ttf-mscorefonts-installer"
+        case ${2} in
+            xfce ) 
+                APT_PACKAGES+=" xfce4-panel xfwm4 xfce4-settings xfce4-session \
+                xfdesktop4 xfconf xfce4-notifyd ristretto tumbler xarchiver \
+                mousepad xfce4-mixer xfce4-volumed thunar thunar-volman \
+                thunar-archive-plugin xfce4-xkb-plugin xfce4-screenshooter \
+                xfce4-power-manager gtk2-engines-xfce gtk3-engines-xfce \
+                xfce4-terminal xfwm4-themes orage xfce4-appfinder xfce4-places-plugin \
+                xfce4-weather-plugin libxfce4ui-utils galculator"
+            ;;
+            kde ) 
+                APT_PACKAGES+=" dolphin kde-l10n-ru kde-window-manager \
+                kde-baseapps-bin kde-workspace-bin polkit-kde-1 oxygencursors \
+                oxygen-icon-theme plasma-widgets-workspace plasma-containments-addons \
+                libkdesu5 kdesudo kde-runtime gwenview systemsettings konsole \
+                kate kmix ark kwalletmanager kfind phonon-backend-gstreamer \
+                ksysguard ksnapshot akonadi-backend-sqlite sqlite3 kcalc"
+            ;;
+            mixed ) 
+                APT_PACKAGES+=" openbox hsetroot gmrun gsimplecal arandr \
+                notification-daemon tint2 gxkb volumeicon-alsa compton spacefm \
+                udevil lilyterm viewnior xarchiver galculator geany scrot"
+            ;;
+        esac
+
+        case ${3} in
+            lightdm )
+                APT_PACKAGES+=" lightdm"
+                if [ ${2} == "kde" ]; then
+                    APT_PACKAGES+=" lightdm-kde-greeter"
+                else
+                    APT_PACKAGES+=" lightdm-gtk-greeter"
+                fi
+            ;;
+            kdm ) APT_PACKAGES+=" kdm" ;;
+        esac
+    fi
 }
 
 # Install packages
 proceed_to_install() {
-    case ${1} in
-        [0]* ) install_base; install_fonts ;;
-        [1]* ) install_base ;;
-        [2]* ) install_fonts ;;
-        * ) echo "Something wrong: installaton aborted."; exit 1;;
-    esac
-
-    read -p "You proceed to install selected packages. Do you want to continue? [Y]es, [N]o (default \"yes\"): " response
+    read -p "You proceed to install \"${PKG_SELECT}\". Do you want to continue? [Y]es, [N]o (default \"yes\"): " response
     case ${response} in
         [Yy]*|"" ) 
             is_apt_configured
@@ -91,15 +139,9 @@ proceed_to_install() {
             exit 1
         ;;
     esac
-
-    echo "xxx"
 }
 
 # Run this script
 runs_as_root
 user_selection
-proceed_to_install ${?}
-
-
-
-    
+proceed_to_install
